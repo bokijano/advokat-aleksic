592b51d55ab4ed3d52d94a2439b4eb510188ff81
Bojan Krstic <krstic.bojan80@gmail.com>
2020-01-22 23:06:57 +0100

    Updates

---
+++ precache-manifest.17a5a37986127a52ddda69e90ee6f1ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "20cfcbeb602a703248bc",
+    "url": "/advokat-aleksic/static/css/main.bd055cf0.chunk.css"
+  },
+  {
+    "revision": "20cfcbeb602a703248bc",
+    "url": "/advokat-aleksic/static/js/main.20cfcbeb.chunk.js"
+  },
+  {
+    "revision": "e77086e3650f463fb2b5",
+    "url": "/advokat-aleksic/static/js/runtime~main.e77086e3.js"
+  },
+  {
+    "revision": "4a242f824681392bcc99",
+    "url": "/advokat-aleksic/static/js/2.4a242f82.chunk.js"
+  },
+  {
+    "revision": "d86bd0ec4c6b5f943f3943c0f788e452",
+    "url": "/advokat-aleksic/static/media/law1.d86bd0ec.jpg"
+  },
+  {
+    "revision": "a8e71d3abcd5da96613ce3c351fa1210",
+    "url": "/advokat-aleksic/static/media/name.a8e71d3a.jpg"
+  },
+  {
+    "revision": "fcee529edb8d0ed2c16a3ace803eb8b8",
+    "url": "/advokat-aleksic/index.html"
+  }
+];
